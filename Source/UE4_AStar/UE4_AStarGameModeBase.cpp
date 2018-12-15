c13701b156feb0dec77f9ccdcf60b253a6eb194d
incfour <incfour@gmail.com>
2018-12-16 01:25:32 +0900

    First Init

---
+++ Source/UE4_AStar/UE4_AStarGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE4_AStar.h"
+#include "UE4_AStarGameModeBase.h"
+
+
+
+
---
+++ Source/UE4_AStar/UE4_AStarGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "UE4_AStarGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_ASTAR_API AUE4_AStarGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
