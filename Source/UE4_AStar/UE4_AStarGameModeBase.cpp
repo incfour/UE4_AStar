734369e204bfe4c381191e36a4f3086f0f5d67d0
incfour <incfour@gmail.com>
2018-12-17 01:53:55 +0900

    Testing

--- Source/UE4_AStar/UE4_AStarGameModeBase.cpp
+++ Source/UE4_AStar/UE4_AStarGameModeBase.cpp
@@ -3,6 +3,25 @@
 #include "UE4_AStar.h"
 #include "UE4_AStarGameModeBase.h"
 
+//AUE4_AStarGameModeBase::AUE4_AStarGameModeBase(const FObjectInitializer& ObjectInitializer)
+//:Super(ObjectInitializer)
+//{
+//	
+//}
+
+void AUE4_AStarGameModeBase::StartPlay()
+{
+	for (TActorIterator<AActor> ActorItr(GetWorld()); ActorItr; ++ActorItr)
+	{
+		// Same as with the Object Iterator, access the subclass instance with the * or -> operators.
+		AActor *Actor = *ActorItr;
+		if (Actor->GetName() == TEXT("CameraActor_1"))
+		{			
+			UE_LOG(LogTemp, Warning, TEXT("1 : %s"), *Actor->GetName())
+			break;
+		}		
+	}
+}
 
 
 
--- Source/UE4_AStar/UE4_AStarGameModeBase.h
+++ Source/UE4_AStar/UE4_AStarGameModeBase.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "GameFramework/GameModeBase.h"
+#include "EngineUtils.h"
 #include "UE4_AStarGameModeBase.generated.h"
 
 /**
@@ -12,8 +13,9 @@ UCLASS()
 class UE4_ASTAR_API AUE4_AStarGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
-	
-	
-	
+
+	//AUE4_AStarGameModeBase(const FObjectInitializer& ObjectInitializer);	
+
+public:
+	virtual void StartPlay() override;
 };
